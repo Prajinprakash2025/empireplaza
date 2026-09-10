9bd10d04bf4b30749ddfdba3f0372e52dfd7207e
Prajinprakash2025 <prajinprakash722@gmail.com>
2026-09-10 21:58:02 +0530

    edit the home section count

--- menu/serializers.py
+++ menu/serializers.py
@@ -70,10 +70,10 @@ class MenuItemSerializer(serializers.ModelSerializer):
 
         # 🌟 2. Section Limits Validation
         SECTION_LIMITS = {
-            'BEST SELLER': 10,
+            'BEST SELLER': 12,
             'BANNER': 4,
-            'COMBO MENU': 9,
-            "TODAY'S SPECIAL": 6,
+            'COMBO MENU': 6,
+            "TODAY'S SPECIAL": 8,
         }
 
         if section in SECTION_LIMITS:
